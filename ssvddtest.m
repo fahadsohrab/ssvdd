2bce91d98becad444dcbb79ce09d1f107434cdde
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-01-29 15:38:15 +0200

    Update ssvddtest.m

--- ssvddtest.m
+++ ssvddtest.m
@@ -5,17 +5,34 @@ function varargout=ssvddtest(Testdata,testlabels,ssvddmodel)
 %   Testdata  = Contains testing data from
 %   Testlabels= contains original test lables
 %   ssvddmodel= contains the output obtained from "ssvddmodel=ssvddtrain(Traindata,varargin)"
-% Output      
+% Output
 %   output argument #1 = predicted labels
-%   output argument #2 = accuracy 
+%   output argument #2 = accuracy
 %   output argument #3 = sensitivity (True Positive Rate)
 %   output argument #4 = specificity (True Negative Rate)
-%   output argument #5 = precision 
+%   output argument #5 = precision
 %   output argument #6 = F-Measure
 %   output argument #7 = Geometric mean i.e, sqrt(tp_rate*tn_rate)
 %Example
 %[predicted_labels,accuracy,sensitivity,specificity]=ssvddtest(Testdata,testlabels,ssvddmodel);
 
+nptflag=ssvddmodel.npt{1};
+if nptflag==1
+    disp('NPT based non-linear S-SVDD Testing...')
+    A=ssvddmodel.npt{2};
+    Ktrain=ssvddmodel.npt{3};
+    Phi=ssvddmodel.npt{4};
+    M_train=ssvddmodel.npt{5};
+    NN = size(Testdata,2);
+    N = size(Ktrain,2);
+    Dtest = ((sum(M_train'.^2,2)*ones(1,NN))+(sum(Testdata'.^2,2)*ones(1,N))'-(2*(M_train'*Testdata)));
+    Ktest = exp(-Dtest/A);
+    M = size(Ktest,2);
+    Ktest = (eye(N,N)-ones(N,N)/N) * (Ktest - (Ktrain*ones(N,1)/N)*ones(1,M));
+    Testdata = pinv(Phi')*Ktest;
+else
+    disp('Linear S-SVDD Testing...')
+end
 Q=ssvddmodel.Q;
 Model=ssvddmodel.modelparam{end};
 RedTestdata=Q{end}* Testdata;
