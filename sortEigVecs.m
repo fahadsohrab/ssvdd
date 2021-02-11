476f9ee27c3319a969a0d1f3ad03d7d3390c0d05
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-11 16:03:38 +0200

    Update sortEigVecs.m

--- sortEigVecs.m
+++ sortEigVecs.m
@@ -1,29 +1,35 @@
+function [mevcs, mevls] = sortEigVecs(evcs,evls,type)
 % Input:
 % evls: not sorted eigenvalues (row vector)
 % evs: not sorted eigenvactors (columns)
-% m: number of eigenvectors/eigenvalues to keep
 %
 % Output:
-% mevcs: maximun eigenvectors in descending order
-% mevls maximun eigenvalues in descending order
-function [mevcs, mevls] = sortEigVecs(evs,evls,type)
+% sorted eigenvalues and eigenvectors
 
 if nargin < 3,  type = 'descend';  end
-nans_found = sum(isnan(evls));      complex_found = sum( imag(evls) ~=0 ); 
-if nans_found~=0,    evls(isnan(evls)) = min(evls)*10^(-1)*ones(nans_found,1);       end % replace NANs with smallest value
-if complex_found~=0, evls(imag(evls)~=0) = min(evls)*10^(-1)* ones(complex_found,1); end % replace complex numbers with smallest value
 
-nans_found = sum(isnan(evs));      complex_found = sum( imag(evs) ~=0 ); 
-if nans_found~=0,    evs(isnan(evs)) = 0;     end % replace NANs with smallest value
-if complex_found~=0, evs(imag(evs)~=0) = 0;   end % replace complex numbers with smallest value
+%Clean
+if isreal(evls)==0
+    evcs = abs(evcs); 
+    evls = abs(evls);
+end
+evls(isinf(evls))=0.0; 
+evls(isnan(evls))=0.0; 
+evcs(isinf(evcs))=0.0; 
+evcs(isnan(evcs))=0.0;
 
+%Round near-zero eigenvalues to zero
+evls(evls<10^-6) = 0.0;
 
-% sort eigenvalues
-[auxv, auxi] = sort(evls,type);     clear auxv; 
+%Sort eigenvalues and remove zeros
+[auxv, auxi] = sort(evls,type);
+mevls = auxv;
+mevcs = evcs(:,auxi);
+mevcs = mevcs(:,mevls > 0);
+mevls = mevls(mevls > 0);
 
-% sort eigenvectors
-for i=1:length(evls);
-    idx = auxi(i); % get index of ith eigenvalue 
-    mevls(:,i) = evls(idx); % get ith eigenvalue
-    mevcs(:,i) = evs(:,idx); % get ith eigenvector
+%If there are no positive eigenvalues, return zeros
+if isempty(mevls)
+    mevls = 0.0;
+    mevcs = zeros(length(evls), 1);
 end
